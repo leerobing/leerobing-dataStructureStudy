6cbb801de006ef594c350126187c7b0b2671be90
leerobing <105533691+leerobing@users.noreply.github.com>
2023-06-03 12:46:57 +0900

    feat: 연결리스트로 스택 구현

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,10 @@
-import ArrayQueue.ArrayQueue;
+import ListStack.NodeManager;
 
 public class Main {
     public static void main(String[] args) {
-
-        ArrayQueue queue = new ArrayQueue(4);
-
-        queue.push(3);
-        queue.push(4);
-        queue.push(5);
-        System.out.println(queue);
-
-        queue.push(7);
-        System.out.println(queue);
-
-        queue.pop();
-        queue.pop();
-        System.out.println(queue);
-
-        queue.push(8);
-        queue.push(9);
-        System.out.println(queue);
-
+        NodeManager nodeManager = new NodeManager();
+        nodeManager.push(4);
+        nodeManager.push(5);
+        System.out.println(nodeManager.peek());
     }
 }
